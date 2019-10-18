e50aae298fdb72b33bb8697fc5a87a831e4190c1
csabbey <csabbey@gmail.com>
2019-10-18 10:29:01 -0400

    Update IdfaPlugin.m
    Getting rid of iOS impl so there's no way the code gets included - Apple might ding us for having the code even if we're not using it.

--- src/ios/IdfaPlugin.m
+++ src/ios/IdfaPlugin.m
@@ -1,19 +1,9 @@
 #import "IdfaPlugin.h"
-#import <AdSupport/ASIdentifierManager.h>
+
 
 @implementation IdfaPlugin
 
 - (void)getInfo:(CDVInvokedUrlCommand *)command {
-    [self.commandDelegate runInBackground:^{
-        NSString *idfaString = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
-        BOOL enabled = [[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled];
-
-        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{
-            @"idfa": idfaString,
-            @"limitAdTracking": [NSNumber numberWithBool:!enabled]
-        }];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
 }
 
 @end
